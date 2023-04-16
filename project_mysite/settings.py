27a81220ffcefa2dd6cdeb8f83910ecac452f341
joaomalfatti <joao.cursos1210@gmail.com>
2023-04-16 13:58:51 -0400

    Foi adicionado uma configuração onde meu site vai mexer com midia, então foi mexindo no seetings, foi adicionado um arquivo, dali que vai ser puxado as imagens

--- project_mysite/settings.py
+++ project_mysite/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
+MEDIA_R00T = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
